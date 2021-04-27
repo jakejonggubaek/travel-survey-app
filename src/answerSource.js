230a09cb36f7e198633311ffa35923914b2ca832
jakejonggubaek <jakejonggubaek@gmail.com>
2021-04-26 23:29:22 -0400

    added style for responsive design

--- src/answerSource.js
+++ src/answerSource.js
@@ -1,4 +1,4 @@
-const localCulture = [['New Orleans', 'Mexico city', 'Cusco', 'Montreal'], ['Portugal', 'Munich', 'Sicily', 'Istanbul'], ['Kyoto', 'Bangkok', 'Delhi', 'Jordan'], ['Morocco', 'Morocco', 'Egypt', 'Egypt']];
+const localCulture = [['Montreal', 'New Orleans', 'Mexico city', 'Cusco'], ['Portugal', 'Munich', 'Sicily', 'Istanbul'], ['Kyoto', 'Bangkok', 'Delhi', 'Jordan'], ['Morocco', 'Morocco', 'Egypt', 'Egypt']];
 
 const relaxing = [['Cuba', 'Key West', 'Belize', 'Turks and Caico'], ['Croatia', 'French Riviera', 'Amalfi', 'Cyprus'], ['Guam', 'Koh Samui', 'Bali', 'Bora Bora'], ['Canary Islands', 'Sharm El Sheikh', 'Zanzibar','Seychelles']];
 
